02f281f748ae1f73be51a58506a96a42fd454ac7
nishantcm <cmnishantwork@gmail.com>
2024-10-23 23:58:43 +0530

    Update the App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import { useState } from 'react';
+
 import Header from './components/Header.jsx';
 import UserInput from './components/UserInput.jsx';
+import Results from './components/Results.jsx';
 
 function App() {
+  const [UserInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturns: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput(prevUserInput => {
+        return {
+            ...prevUserInput,
+            [inputIdentifier]: newValue
+        };
+    });
+  }
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput UserInput={UserInput} onChange={handleChange}/>
+      <Results input={UserInput} />
     </>
   )
 }
