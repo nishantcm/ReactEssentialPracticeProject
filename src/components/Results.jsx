c54843a9af8f44f12a8d302c4349df7e74a44bc7
nishantcm <cmnishantwork@gmail.com>
2024-10-26 00:27:04 +0530

    Update the Results.jsx file

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,12 +1,16 @@
-import { calculateInvestmentResults } from '../util/investment.js';
+import { calculateInvestmentResults, formatter } from '../util/investment.js';
 
 export default function Results({ input }) {
     const resultsData = calculateInvestmentResults(input);
+    const initialInvestment = 
+        resultsData[0].valueEndOfYear - 
+        resultsData[0].interest -
+        resultsData[0].annualInvestment;
 
     console.log(resultsData);
 
     return (
-        <table id="results">
+        <table id="result">
             <thead>
                 <tr>
                     <th>Year</th>
@@ -18,9 +22,22 @@ export default function Results({ input }) {
             </thead>
             <tbody>
                 {resultsData.map(yearData => {
-                    return <tr key={yearData.yaer}>
-                        <td></td>
-                    </tr>
+                    const totalInterest = 
+                        yearData.valueEndOfYear - 
+                        yearData.annualInvestment * yearData.year
+                        initialInvestment;
+
+                    const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
+
+                    return (
+                        <tr key={yearData.year}>
+                            <td>{yearData.year}</td>
+                            <td>{formatter.format(yearData.valueEndOfYear)}</td>
+                            <td>{formatter.format(yearData.interest)}</td>
+                            <td>{formatter.format(totalInterest)}</td>
+                            <td>{formatter.format(totalAmountInvested)}</td>
+                        </tr>
+                    );
                 })}
             </tbody>
         </table>
