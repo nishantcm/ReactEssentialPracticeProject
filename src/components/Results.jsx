57399b82ec4bf7d605aa981b9c55fa38bc0852c6
nishantcm <cmnishantwork@gmail.com>
2024-10-25 23:54:12 +0530

    Update the Results.jsx file

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -5,5 +5,24 @@ export default function Results({ input }) {
 
     console.log(resultsData);
 
-    return <p>Results...</p>
+    return (
+        <table id="results">
+            <thead>
+                <tr>
+                    <th>Year</th>
+                    <th>Investment Value</th>
+                    <th>Intrest (Year)</th>
+                    <th>Total Interest</th>
+                    <th>Invested Capital</th>
+                </tr>
+            </thead>
+            <tbody>
+                {resultsData.map(yearData => {
+                    return <tr key={yearData.yaer}>
+                        <td></td>
+                    </tr>
+                })}
+            </tbody>
+        </table>
+    );
 }
